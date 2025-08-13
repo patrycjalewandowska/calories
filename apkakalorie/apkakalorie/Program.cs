92e5895f3f467755955b5b2e6a44ed19abc54ddd
patrycjalewandowska <24plewandowska@gmail.com>
2025-08-13 13:34:50 +0200

    add backOptionMenu

--- apkakalorie/apkakalorie/Program.cs
+++ apkakalorie/apkakalorie/Program.cs
@@ -6,25 +6,10 @@ using apkakalorie.ConsoleUI;
 Context context = new Context();
 
 ServiceMenu listOptionMenu = new ServiceMenu();
-
-listOptionMenu.AddMenuToList(1, "Sekcja przepisow");
-listOptionMenu.AddMenuToList(2, "Sekcja produktow");
-
 ServiceMenu listRecipeMenu = new ServiceMenu();
-
-listRecipeMenu.AddMenuToList(1, "Zobacz Przepis");
-listRecipeMenu.AddMenuToList(2, "Utworz nowy przepis");
-listRecipeMenu.AddMenuToList(3, "Zaktualizuj przepis");
-listRecipeMenu.AddMenuToList(4, "Usun przepis");
-listRecipeMenu.AddMenuToList(5, "Powrot");
-
 ServiceMenu listProductMenu = new ServiceMenu();
 
-listProductMenu.AddMenuToList(1, "Zobacz Produkt");
-listProductMenu.AddMenuToList(2, "Utworz nowy produkt");
-listProductMenu.AddMenuToList(3, "Zaktualizuj produkt");
-listProductMenu.AddMenuToList(4, "Usun produkt");
-listProductMenu.AddMenuToList(5, "Powrot");
+AddAllMenu(listOptionMenu, listRecipeMenu, listProductMenu);
 
 ServiceProduct productService = new ServiceProduct(context);
 ServiceRecipe recipeService = new ServiceRecipe(context);
@@ -55,7 +40,7 @@ while (true)
             case 2: Console.WriteLine("dodaj"); break;
             case 3: Console.WriteLine("update"); break;
             case 4: Console.WriteLine("usun"); break;
-            case 5: Console.WriteLine("powrot"); break;
+            case 5: ShowSubMenu(listOptionMenu); break;
             default:
                 Console.WriteLine("Brak dostepnego Menu"); break;
         }
@@ -70,10 +55,29 @@ while (true)
         case 2: ProductConsoleUI.CreateProductFromUserInput(productService);  break;
         case 3: Console.WriteLine("update"); break;
         case 4: ProductConsoleUI.DeleteProductById(productService);   break;
-        case 5: Console.WriteLine("powrot"); break;
+        case 5: ShowSubMenu(listOptionMenu); break;
         default:
             Console.WriteLine("Brak dostepnego Menu"); break;
 
         }
     }
+}
+
+
+ void AddAllMenu(ServiceMenu listOptionMenu, ServiceMenu listRecipeMenu,ServiceMenu listProductMenu) 
+{
+    listOptionMenu.AddMenuToList(1, "Sekcja przepisow");
+    listOptionMenu.AddMenuToList(2, "Sekcja produktow");
+
+    listRecipeMenu.AddMenuToList(1, "Zobacz Przepis");
+    listRecipeMenu.AddMenuToList(2, "Utworz nowy przepis");
+    listRecipeMenu.AddMenuToList(3, "Zaktualizuj przepis");
+    listRecipeMenu.AddMenuToList(4, "Usun przepis");
+    listRecipeMenu.AddMenuToList(5, "Powrot");
+
+    listProductMenu.AddMenuToList(1, "Zobacz Produkt");
+    listProductMenu.AddMenuToList(2, "Utworz nowy produkt");
+    listProductMenu.AddMenuToList(3, "Zaktualizuj produkt");
+    listProductMenu.AddMenuToList(4, "Usun produkt");
+    listProductMenu.AddMenuToList(5, "Powrot");
 }
